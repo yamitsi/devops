59e2707d67b6020c8cedade6a3bb91b40c0c8747
Omri Siri <omrisiri@gmail.com>
2018-05-02 17:11:24 +0300

    install flannel instead of calico

--- k8s/install.sh
+++ k8s/install.sh
@@ -1,4 +1,5 @@
 curl -fsSl https://get.docker.com | sh
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
@@ -6,7 +7,8 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 
-kubeadm init --apiserver-advertise-address 10.10.0.11 --pod-network-cidr=192.168.0.0/16
-kubectl --kubeconfig /etc/kubernetes/admin.conf apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+
+kubeadm init 
+kubectl --kubeconfig /etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
 
 kubectl --kubeconfig /etc/kubernetes/admin.conf taint nodes --all node-role.kubernetes.io/master-
