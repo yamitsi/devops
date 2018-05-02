7af3a90ee9535ae9a7fecff674567c9176da7b6e
yamitsi <37380483+yamitsi@users.noreply.github.com>
2018-05-02 19:02:08 +0300

    Update install.sh
    adding comments

--- k8s/install.sh
+++ k8s/install.sh
@@ -1,3 +1,7 @@
+#k8s install script
+
+#install on master and slave:
+
 curl -fsSl https://get.docker.com | sh
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -7,8 +11,11 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 
+#install on master only
 
+#run master services
 kubeadm init 
 kubectl --kubeconfig /etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
 
-kubectl --kubeconfig /etc/kubernetes/admin.conf taint nodes --all node-role.kubernetes.io/master-
+#optional - enables running pods on master - not recommended for production!
+#kubectl --kubeconfig /etc/kubernetes/admin.conf taint nodes --all node-role.kubernetes.io/master-
